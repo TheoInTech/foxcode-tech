65cd17c44f4afd2d973dd1b45e6f128ea5965dea
Theo Roque <roquevtr@gmail.com>
2020-01-13 20:50:06 +0800

    Merged v1.0.0 to master

--- src/App/components/Navbar/Navbar.jsx
+++ src/App/components/Navbar/Navbar.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { TabNavigation, Tab } from 'evergreen-ui'
+import { Pane, TabNavigation, Tab } from 'evergreen-ui'
+import Component from "@reach/component-component";
+import './Navbar.scss';
 
 const Navbar = () => {
-    const links = [
-        {
-            link: '/',
-            label: 'Home'
-        },
-        {
-            link: '/about-us',
-            label: 'About Us'
-        },
-        {
-            link: '/projects',
-            label: 'Projects'
-        },
-        {
-            link: '/contact',
-            label: 'Contact'
-        }
-    ];
-
     return (
-        <TabNavigation>
-            {links.map((tab, index) => (
-                <Tab key={`menu-${tab.label}`} is="a" href={tab.link} id={tab} isSelected={index === 0}>
-                    {tab.label}
-                </Tab>
-            ))}
-        </TabNavigation>
+        <Pane display="flex" flexWrap="wrap" justifyContent="flex-end" flexBasis="100%" className="main__navbar">
+            <Component
+                initialState={{
+                    selectedIndex: 0,
+                    tabs: [
+                        {
+                            link: '/',
+                            label: 'Home'
+                        },
+                        {
+                            link: '/about-us',
+                            label: 'About Us'
+                        },
+                        {
+                            link: '/projects',
+                            label: 'Projects'
+                        },
+                        {
+                            link: '/contact',
+                            label: 'Contact'
+                        }
+                    ]
+                }}
+            >
+                {({ state, setState }) => (
+                    <TabNavigation className="main__navbar__tab">
+                        {state.tabs.map((tab, index) => (
+                            <Tab key={tab.label} id={tab.label} is="a" href={tab.link} isSelected={index === 0} onSelect={() => setState({ selectedIndex: index })} height={100}>
+                                {tab.label}
+                            </Tab>
+                        ))}
+                    </TabNavigation>
+                )}
+            </Component>
+        </Pane>
     );
 };
 
